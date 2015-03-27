e9f1fe1dd3717b25cfbe4e177f357ab48d920f76
Daniel Subauste Oliden <daalsuol@gmail.com>
2015-03-27 17:54:31 -0500

    Create excepciones03.rb

---
+++ clase07/excepciones03.rb
@@ -0,0 +1,23 @@
+def explode
+ raise "boom" if rand(10) == 0
+end
+
+def risky
+  begin
+    10.times do
+      explode
+    end
+   rescue TypeError
+     puts $!
+   end
+   "hello"
+end 
+
+def defuse
+  begin
+    puts risky
+  rescue RuntimeError => e
+    puts e.message
+  end
+end
+defuse
